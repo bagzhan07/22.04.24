3900f23bca99509a94e33abd602979afb5978e44
bagzhan07 <168531302+bagzhan07@users.noreply.github.com>
2024-04-30 15:38:20 +0500

    Add files via upload

---
+++ mycode.py
@@ -0,0 +1,32 @@
+def add(x,y):
+    return x+y
+a = add(4,5)
+print(a)
+
+
+def append_to_list(x):
+    list1 = [1,2,3,4]
+    list1.append(x)
+    return list1
+print(append_to_list(5))
+
+def add_to_list(x):
+    list_1 = [
+        {
+            "name": "Arman",
+            "last_name": "Doszhanov",
+            "age": 20
+        },
+        {
+            "name": "Askar",
+            "last_name": "Ecbolov",
+            "age": 15
+        },
+        {
+            "name": "Zhandos",
+            "last_name": "Muratov",
+            "age": 45
+        }
+]
+
+    
